725f0aae846704569bfc245ef9e228c21a790cc2
Anthony <86032299+Anthony9933@users.noreply.github.com>
2024-04-16 10:41:37 -0300

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,37 @@
-#first codes
+import streamlit as st
+import pandas as pd
+
+# Sidebar (Menu Lateral)
+page = st.sidebar.selectbox("Escolha a Página", ["Visão Geral", "Filtros e Dados"])
+
+def show_overview():
+    
+    # Visão Geral do Projeto
+    st.header("Visão Geral do Projeto")
+    st.write("Bem-vindo ao projeto de Visualização de dados de acidentes registrados pela Polícia Rodoviária Federal! "
+             "Este projeto gira em torno da análise e apresentação dos dados coletados. O conjunto de dados contém várias colunas fornecendo insights "
+             "sobre os produtos e informações relativas aos acidentes, incluindo data, dia da semana, estado, município e causa.")
+    
+    # Como Funciona
+    st.header("Como Funciona")
+    st.write("O projeto utiliza um conjunto de dados com informações sobre os acidentes. Aqui está uma breve visão "
+             "geral dos principais componentes:")
+    
+    # Objetivo do Projeto
+    st.header("Objetivo do Projeto")
+    st.write("O principal objetivo deste projeto é obter insights pela análise dos dados e gráficos criados. Isso inclui entender "
+             "o que os dados estão nos dizendo, identificar e explorar padrões.")
+    
+    # Como Utilizar
+    st.header("Como Utilizar")
+    st.write("Para explorar o projeto, você pode navegar por diferentes seções usando a barra lateral. As principais seções incluem:")
+      
+    st.header("Conclusão")
+    st.write("Sinta-se à vontade para analisar o conjunto de dados, obter insights e tirar conclusões significativas a partir "
+               "dos dados apresentados. Para análises específicas ou dúvidas, novos recursos podem ser incorporados com base nos "
+               "objetivos do seu projeto.")
+  
+    st.write("Aproveite a exploração do projeto visualização dos dados da PRF!")
+# Página de Visão Geral
+if page == "Visão Geral":
+    show_overview()
